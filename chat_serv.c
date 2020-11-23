64051e2264a7e117377fc1e07b628fb5d2d07269
BloodyNeonz <59564149+BloodyNeonz@users.noreply.github.com>
2020-11-23 17:48:04 +0900

    correct (testBranch used)
    test submit

--- chat_serv.c
+++ chat_serv.c
@@ -194,7 +194,7 @@ int main(int argc, char *argv[])
                             strcpy(buf.message, "******< ");
                             strcat(buf.message, member_list[i].name);
                             strcat(buf.message, " > 님이 나가셨습니다.******");
-                            for (int j = 0; j < fd_max + 2; j++)
+                            for (int j = 0; j < fd_max + 1; j++)
                             {
                                 if (member_list[j].room == room)
                                 {
