06235e74b30876b2bbe5f8e17be7e7c0656ddd5f
jaehee9231 <72240056+jaehee9231@users.noreply.github.com>
2020-11-23 19:25:42 +0900

    print name
    print my name

--- hello Github.c
+++ hello Github.c
@@ -4,5 +4,10 @@ int main(void){
 	printf("Hello Github\n");
 	//아래에는 조원 네명이 하나씩 추가할 겁니다.
 	//양식은 printf("Hello 이름");
+<<<<<<< Updated upstream
 	printf("한지예");
+=======
+	//테스트
+	printf("정재희\n");
+>>>>>>> Stashed changes
 }
